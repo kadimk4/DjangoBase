de9fb65960f0abe8d2b2d7829fd905e3d52aea5f
kadimk4 <khaydasrhin2007@gmail.com>
2024-06-13 12:52:53 +0300

    work

--- core/project/urls.py
+++ core/project/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.apps.users.urls'))
+    path('admin/', admin.site.urls)
 ]
